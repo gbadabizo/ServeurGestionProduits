63a4228e4479f93ca4ae0ba18296603c8cb3072a
EDEM GBADABIZO <edemgbadabizo@gmail.com>
2018-12-12 18:08:37 +0000

    first commit

---
+++ src/main/java/com/all4tic/ServeurGestionProduits/ServeurGestionProduitsApplication.java
@@ -0,0 +1,14 @@
+package com.all4tic.ServeurGestionProduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionProduitsApplication.class, args);
+	}
+
+}
+
